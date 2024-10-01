1308d68aadf2d03720f5a22054af33cf7e2ad5ce
rajacloud2244 <spandanapaladi@outlook.com>
2024-10-01 16:55:23 +0530

    Add files via upload

---
+++ backend.tf
@@ -0,0 +1,17 @@
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+  backend "azurerm" {
+      resource_group_name  = "tf"
+      storage_account_name = "tf224466"
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
+  }
+
+}
+

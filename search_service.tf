b928749108196bb53019a4b9abf8277cb0becc9b
rajacloud2244 <spandanapaladi@outlook.com>
2024-10-03 16:26:23 +0530

    Update search_service.tf

--- search_service.tf
+++ search_service.tf
@@ -1,5 +1,5 @@
 module "cognitive_search" {
-  source = "git::https://rajacloud2244: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@github.com/rajacloud2244/StandardModules.git//cognitive_search_service"
+  source = "git::https://rajacloud2244:${{ secrets.PAT_TOKEN }}@github.com/rajacloud2244/StandardModules.git//cognitive_search_service"
   search_service_name             = local.search_service_name
   resource_group_name = local.resource_group_name
   location            = local.location

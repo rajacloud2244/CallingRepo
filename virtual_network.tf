023014c019c24f79e3e2dce4675a6471c9410b49
rajacloud2244 <spandanapaladi@outlook.com>
2024-10-03 16:32:27 +0530

    Update virtual_network.tf

--- virtual_network.tf
+++ virtual_network.tf
@@ -1,5 +1,5 @@
 module "virtual_network" {
-  source = "git::https://rajacloud2244:${{ secrets.PAT_TOKEN }}@github.com/rajacloud2244/StandardModules.git//virtual_network"
+  source = "git::https://rajacloud2244:${{ var.PAT_TOKEN }}@github.com/rajacloud2244/StandardModules.git//virtual_network"
   virtual_network_name = local.virtual_network_name
   location            = local.location
   resource_group_name = local.resource_group_name 

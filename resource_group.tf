c94ec4f2a19b4cef117f9949cce29d398f275cd3
rajacloud2244 <spandanapaladi@outlook.com>
2024-10-03 16:37:57 +0530

    Update resource_group.tf

--- resource_group.tf
+++ resource_group.tf
@@ -1,5 +1,5 @@
 module "resource_group" {
-  source = "git::https://rajacloud2244:${{ secrets.PAT_TOKEN }}@github.com/rajacloud2244/StandardModules.git//resource_group"
+  source = "git::https://rajacloud2244:${var.git_token}@github.com/rajacloud2244/StandardModules.git//resource_group"
   resource_group_name = local.resource_group_name
   location            = local.location
   managed_by = local.managed_by

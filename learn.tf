038c61e734b4e7f36375addc973e981a68de73c1
rajacloud2244 <spandanapaladi@outlook.com>
2024-10-04 16:15:51 +0530

    Update learn.tf

--- learn.tf
+++ learn.tf
@@ -10,3 +10,15 @@ output "greeting" {
   value = "Hello, ${var.name}! Welcome to your new adventure!"
 }
 
+
+# Define a variable for the list of fruits
+variable "fruits" {
+  description = "A list of fruits"
+  type        = list(string)
+  default     = ["Apple", "Banana", "Cherry", "Date"]
+}
+
+# Output the list of fruits
+output "fruit_list" {
+  value = var.fruits
+}

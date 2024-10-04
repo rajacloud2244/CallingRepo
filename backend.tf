3c12097b94a6da9692f3abf1bde5cdb2c4db5674
rajacloud2244 <spandanapaladi@outlook.com>
2024-10-04 16:26:13 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -10,7 +10,7 @@ terraform {
       resource_group_name  = "tf"
       storage_account_name = "tf224466"
       container_name       = "learn"
-      key                  = "terraform.tfstate"
+      key                  = "terraform.tfstate"       #do not write name in azue portal, it will create automatically
   }
 
 }

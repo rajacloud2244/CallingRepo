901e4085b378ef82f47296687b5017d066e66d95
rajacloud2244 <spandanapaladi@outlook.com>
2024-10-04 16:06:29 +0530

    Update learn.tf

--- learn.tf
+++ learn.tf
@@ -1,1 +1,12 @@
+# String
+variable "name" {
+  description = "The name of the person"
+  type        = string
+  default     = "Raja"
+}
+
+# Create a greeting message
+output "greeting" {
+  value = "Hello, ${var.name}! Welcome to your new adventure!"
+}
 

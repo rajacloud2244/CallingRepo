500217148670b4cd5680c973f3815c8fc98cd14d
rajacloud2244 <spandanapaladi@outlook.com>
2024-10-04 13:39:39 +0530

    Delete cognitive_account.tf

--- cognitive_account.tf
+++
@@ -1,8 +0,0 @@
-module "cognitive_account" {
-   source = "git::https://rajacloud2244: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@github.com/rajacloud2244/StandardModules.git//cognitive_account"
-  location                            = local.location
-  resource_group_name                 = local.resource_group_name
-  cognitive_account_name              = local.cognitive_account_name  
-  sku_name                            = local.sku_name_cognitive_account
-  kind                                = local.cognitivekind
-}

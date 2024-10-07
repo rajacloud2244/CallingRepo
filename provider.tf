077ad261caeae4649966468bc91daf7376459946
rajacloud2244 <spandanapaladi@outlook.com>
2024-10-07 17:38:15 +0530

    Delete provider.tf

--- provider.tf
+++
@@ -1,7 +0,0 @@
-provider "azurerm" {
-  features {}
-   subscription_id = "3b8fd9b2-bd90-4846-869c-bd2bb6aef21a"
-   client_id       = "0e4acfcf-c8de-471a-9a47-4b51d3b5aa2a"
-   client_secret   = "wTZ8Q~jDjTWZ7D-QvXmym2zNOaRheQpm2aWRnbEm"
-   tenant_id       = "40b1ead9-fafd-4978-96db-aaf5baa20bab"
-}
